792b1257ea4006f284bf5c76f8793ce3a65c02e9
rodrigosenna <rodrigo.sonic@gmail.com>
2018-07-27 19:22:12 -0300

    14.4EXERCÍCIOS:ATIVANDOOSPROFILESPARATESTAR

--- src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
@@ -1,12 +1,15 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.casadocodigo.loja.models.BookType;
 import br.com.casadocodigo.loja.models.Product;
 
 @Repository
@@ -27,5 +30,13 @@ public class ProductDAO {
 		return manager.createQuery(
 				"select	distinct(p)	from	Product	p	join	fetch	p.prices	where	p.id	=	:id",
 				Product.class).setParameter("id", id).getSingleResult();
+	}
+
+	public BigDecimal sumPricesPerType(BookType bookType) {
+		TypedQuery<BigDecimal> query = manager.createQuery(
+				"select	sum(price.value)	from	Product	p	join	p.prices	price	where	price.bookType	=	:bookType",
+				BigDecimal.class);
+		query.setParameter("bookType", bookType);
+		return query.getSingleResult();
 	}
 }
